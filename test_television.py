65a0d2118ec921bc43fbdf568c134e6d3935101a
Werd6 <137083014+Werd6@users.noreply.github.com>
2024-11-24 20:05:09 -0600

    Update test_television.py
    Updated to match the formatting from the lecture

--- test_television.py
+++ test_television.py
@@ -1,67 +1,71 @@
 import pytest
 from television import Television  # Assuming your Television class is in a file named television.py
 
-def test_init():
-    """Test the initialization of the Television class."""
-    tv = Television()
-    assert tv._Television__status == False
-    assert tv._Television__muted == False
-    assert tv._Television__channel == Television.MIN_CHANNEL
-    assert tv._Television__volume == Television.MIN_VOLUME
+class TestTelevision:
+    def setup_method(self):
+        """Setup method to initialize the Television object."""
+        self.tv = Television()
 
-def test_power():
-    """Test the power method."""
-    tv = Television()
-    tv.power()
-    assert tv._Television__status == True
-    tv.power()
-    assert tv._Television__status == False
+    def teardown_method(self):
+        """Teardown method to clean up the Television object."""
+        del self.tv
 
-def test_mute():
-    """Test the mute method."""
-    tv = Television()
-    tv.power()  # Turn on the TV to enable muting
-    tv.mute()
-    assert tv._Television__muted == True
-    tv.mute()
-    assert tv._Television__muted == False
+    def test_init(self):
+        """Test the initialization of the Television class."""
+        assert self.tv._Television__status == False
+        assert self.tv._Television__muted == False
+        assert self.tv._Television__channel == Television.MIN_CHANNEL
+        assert self.tv._Television__volume == Television.MIN_VOLUME
 
-def test_channel_up():
-    """Test the channel_up method."""
-    tv = Television()
-    tv.power()  # Turn on the TV
-    tv.channel_up()
-    assert tv._Television__channel == 1
-    tv.channel_up()
-    tv.channel_up()
-    tv.channel_up()  # Should wrap around to the first channel
-    assert tv._Television__channel == Television.MIN_CHANNEL
+    def test_power(self):
+        """Test the power method."""
+        self.tv.power()
+        assert self.tv._Television__status == True
+        self.tv.power()
+        assert self.tv._Television__status == False
 
-def test_channel_down():
-    """Test the channel_down method."""
-    tv = Television()
-    tv.power()  # Turn on the TV
-    tv.channel_down()  # Should wrap around to the last channel
-    assert tv._Television__channel == Television.MAX_CHANNEL
-    tv.channel_down()
-    assert tv._Television__channel == Television.MAX_CHANNEL - 1
+    def test_mute(self):
+        """Test the mute method."""
+        self.tv.power()  # Turn on the TV to enable muting
+        self.tv.mute()
+        assert self.tv._Television__muted == True
+        self.tv.mute()
+        assert self.tv._Television__muted == False
 
-def test_volume_up():
-    """Test the volume_up method."""
-    tv = Television()
-    tv.power()  # Turn on the TV
-    tv.volume_up()
-    assert tv._Television__volume == Television.MIN_VOLUME + 1
-    tv.volume_up()
-    tv.volume_up()  # Should stop at MAX_VOLUME
-    assert tv._Television__volume == Television.MAX_VOLUME
+    def test_channel_up(self):
+        """Test the channel_up method."""
+        self.tv.power()  # Turn on the TV
+        self.tv.channel_up()
+        assert self.tv._Television__channel == Television.MIN_CHANNEL + 1
 
-def test_volume_down():
-    """Test the volume_down method."""
-    tv = Television()
-    tv.power()  # Turn on the TV
-    tv.volume_down()  # Should not go below MIN_VOLUME
-    assert tv._Television__volume == Television.MIN_VOLUME
-    tv._Television__volume = Television.MAX_VOLUME  # Set volume to max for testing
-    tv.volume_down()
-    assert tv._Television__volume == Television.MAX_VOLUME - 1
+        # Wrap around to the first channel
+        for _ in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL):
+            self.tv.channel_up()
+        assert self.tv._Television__channel == Television.MIN_CHANNEL
+
+    def test_channel_down(self):
+        """Test the channel_down method."""
+        self.tv.power()  # Turn on the TV
+        self.tv.channel_down()  # Wrap around to the last channel
+        assert self.tv._Television__channel == Television.MAX_CHANNEL
+
+        self.tv.channel_down()
+        assert self.tv._Television__channel == Television.MAX_CHANNEL - 1
+
+    def test_volume_up(self):
+        """Test the volume_up method."""
+        self.tv.power()  # Turn on the TV
+
+        # Increment volume until the max
+        for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
+            self.tv.volume_up()
+        assert self.tv._Television__volume == Television.MAX_VOLUME
+
+    def test_volume_down(self):
+        """Test the volume_down method."""
+        self.tv.power()  # Turn on the TV
+
+        # Decrement volume until the min
+        for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
+            self.tv.volume_down()
+        assert self.tv._Television__volume == Television.MIN_VOLUME
